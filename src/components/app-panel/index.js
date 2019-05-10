22e2ac46991f4bcebedad82545d61279f1bb3344
Damien McMahon <damien.mcmahon@zonedigital.com>
2019-05-10 12:33:50 +0100

    App Panel styling is looking better

--- src/components/app-panel/index.js
+++ src/components/app-panel/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 import './styles.scss';
 
 const AppPanel = ({name, children}) => (
-    <div className="app__panel-wrapper">
+    <div className="panel__wrapper">
         {name && <h2 className="panel__header">{name}</h2>}
         <div className="panel__content-container">
             {children}
