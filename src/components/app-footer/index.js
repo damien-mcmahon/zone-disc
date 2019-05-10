569d61c443cb19a8a9d19e3c3c0350ac05abd071
Damien McMahon <damien.mcmahon@zonedigital.com>
2019-05-10 11:43:14 +0100

    WIP - App shell colors are there, footer content is in, styling in prog..

--- src/components/app-footer/index.js
+++ src/components/app-footer/index.js
@@ -1,8 +1,34 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import './styles.scss';
+
+const FOOTER_NAV_ITEMS = [
+    {path: '/contact', label: 'Contact Us'},
+    {path: '/help', label: 'Help'},
+    {path: '/terms', label: 'Terms of Use'},
+    {path: '/privacy-policy', label: 'Privacy Policy'}
+];
 
 const AppFooter = () => (
     <footer className="app__footer-wrapper">
-        <h2>FOOTER</h2>
+        <div className="footer__copyright-notice">
+            &copy; 2019 DFS Services LLC
+        </div>
+
+        <nav className="footer__nav-wrapper">
+            {FOOTER_NAV_ITEMS.map(item => (
+                <div className="footer__nav-item-wrapper">
+                    <Link 
+                        className="footer__nav-link"
+                        to={item.path}>{item.label}</Link>
+                </div>
+            ))}            
+        </nav>
+
+        <div className="footer__brand-wrapper">
+
+        </div>
     </footer>
 );
 
--- src/components/app-footer/stories.js
+++ src/components/app-footer/stories.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
+import StoryRouter from 'storybook-react-router';
 
 import AppFooter from './index';
+import { StyleguideWrapper } from '../../stories/decorators';
 
 storiesOf('Components/App/Footer', module)
+    .addDecorator(StoryRouter())
+    .addDecorator(StyleguideWrapper)
     .add('Default', () => <AppFooter />)
