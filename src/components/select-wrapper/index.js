f42b519d49cbdb60237cd06fbc4a06f9ab77d36e
Damien McMahon <damien.mcmahon@zonedigital.com>
2019-05-14 11:08:29 +0100

    Select Wrapper using classnames and proptypes

--- src/components/select-wrapper/index.js
+++ src/components/select-wrapper/index.js
@@ -1,27 +1,46 @@
 import React from 'react';
 import {ErrorMessage} from 'formik';
 import Select from 'react-select';
+import { any, boolean, string, shape } from 'prop-types';
+import classNames from 'classnames';
 
 import './styles.scss';
 
-const SelectWrapper = ({name, label, options, errors, ...props}) => (
-    <div className="select-wrapper__wrapper">
-        <label className="select-wrapper__label" htmlFor={name}>{label}</label>
+const SelectWrapper = ({name, label, options, errors, required = false, ...props}) => {
+    const wrapperClassNames = classNames('select-wrapper__wrapper', {
+        '--required': required
+    });
 
-        <Select 
-            {...props} 
-            name={name} 
-            options={options} 
-            className="select-wrapper__input"
-            classNamePrefix="select-component"
-            onChange={({value}) => props.onChange && props.onChange(value)} />
+    return (
+        <div className={wrapperClassNames}>
+            <label className="select-wrapper__label" htmlFor={name}>{label}</label>
 
-        <ErrorMessage name={name}>
-            {message => 
-                <div className="select-wrapper__error">{message}</div>
-            }
-        </ErrorMessage>
-    </div>
-);
+            <Select 
+                {...props} 
+                name={name} 
+                options={options} 
+                className="select-wrapper__input"
+                classNamePrefix="select-component"
+                onChange={({value}) => props.onChange && props.onChange(value)} />
+
+            <ErrorMessage name={name}>
+                {message => 
+                    <div className="select-wrapper__error">{message}</div>
+                }
+            </ErrorMessage>
+        </div>
+    );
+};
+
+SelectWrapper.propTypes = {
+    name: string.isRequired,
+    label: string.isRequired,
+    options: shape({
+        label: string,
+        value: any
+    }),
+    errors: string,
+    required: boolean
+};
 
 export default SelectWrapper;
